c6b6ec3f1f2289a932e0b1da8d10a00bc5b34e9d
hsbmaulana <hsbmaulana.iso@gmail.com>
2024-01-10 13:40:36 +0700

    Initial Codebase : Configuration

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@
 const nextConfig = {
 
     reactStrictMode: true,
+
+    // https://stackoverflow.com/questions/64261029/next-js-env-vs-serverruntimeconfig
+    // https://nextjs.org/docs/pages/api-reference/next-config-js/runtime-configuration
+
+    serverRuntimeConfig: {
+
+        secret: process.env.SECRET,
+    },
+
+    publicRuntimeConfig: {
+
+        appName: process.env.NEXT_PUBLIC_APP_NAME || "codebase",
+        env: process.env.NEXT_PUBLIC_APP_ENV || "production",
+    },
 };
 
 module.exports = nextConfig;
