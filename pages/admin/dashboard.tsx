039978cde0e009d1a052f15ea8f18fe3c88c6366
hsbmaulana <hsbmaulana.iso@protonmail.com>
2024-06-03 02:40:30 +0700

    Auth : Template

--- pages/admin/dashboard.tsx
+++ pages/admin/dashboard.tsx
@@ -7,7 +7,9 @@ const Template = () =>
     return (
 
         <Fragment>
-            <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400">Test</button>
+            <div className="container m-2">
+                <button type="button" className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400">Sign out</button>
+            </div>
         </Fragment>
     );
 };
